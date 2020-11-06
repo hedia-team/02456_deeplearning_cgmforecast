1f533afc0c0f43fae857e41aa954b1ddbd03a046
Mads Obdrup Jakobsen <mads@hedia.co>
2020-11-06 13:42:41 +0100

    Update .gitignore

--- .config.template.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-data_path = Path.home() / 'Documents' / 'Hedia_local' / 'deeplearning_02456_cgm' / 'data'
-code_path = Path.home() / 'Documents' / 'Hedia_local' / 'deeplearning_02456_cgm'
-model_path = Path.home() / 'Documents' / 'Hedia_local' / 'deeplearning_02456_cgm' / 'trained_models'
-figure_path = Path.home() / 'Documents' / 'Hedia_local' / 'deeplearning_02456_cgm' / 'figs'
